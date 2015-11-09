d08546d6e7d095182e7acec73d0c576b1ade890f
Lei Chen <leochen4891@gmail.com>
2015-11-08 22:58:14 -0800

    fixed mistakes in test data

---
+++ testdata/compare.sh
@@ -0,0 +1,8 @@
+diff -q UnionQueryResult.csv UnionQueryOutput.csv 
+diff -q ConvexHullOutput.csv ConvexHullResult.csv 
+diff -q FarthestPairOutput.csv FarthestPairResult.csv 
+diff -q ClosestPairOutput.csv ClosestPairResult.csv 
+diff -q RangeQueryResult.csv RangeQueryOutput.csv 
+diff -q JoinQueryRectangleResult.csv JoinQueryOutput.csv 
+diff -q JoinQueryPointResult.csv JoinQueryOutput.csv 
+
